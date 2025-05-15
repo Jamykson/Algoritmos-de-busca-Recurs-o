56f2865225fe107b54308c2ff0bf71379bed98c4
Jamykson Freitas <105256874+Jamykson@users.noreply.github.com>
2025-05-15 21:19:15 +0000

    tentando

--- src_cpp/busca_binaria.cpp
+++ src_cpp/busca_binaria.cpp
@@ -1,5 +1,35 @@
 #include "busca_binaria.hpp"
 
-int busca_binaria() {
+int errada = 5;
+
+bool isBadVersion(int versao){
+
+    return versao>=errada;
 
 }
+
+
+int busca_binaria(int n) {
+    int inicio = 1, fim = n, resultado = n, meio;
+
+    while(inicio<=fim){
+        meio = fim/2;
+
+        if(isBadVersion(meio)){
+            resultado = meio;
+            fim = meio - 1;
+        }else{
+            inicio = meio+1;
+
+        }
+
+    }
+
+    
+    
+
+}
+
+/*Ela deve retornar a primeira versão defeituosa entre 1 e n, 
+usando busca binária e uma função isBadVersion(int version) que 
+já te informa se uma versão é defeituosa ou não.*/
